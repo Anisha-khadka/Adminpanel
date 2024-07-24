5baa35b13e751bb367c348f611609667258fec03
Anisha-khadka <anishakhadka434@gmail.com>
2024-07-24 07:02:51 +0545

    Commit

--- adminpanel/src/Pages/Dashboard.tsx
+++ adminpanel/src/Pages/Dashboard.tsx
@@ -1,7 +1,31 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid, Table, TableCaption, TableContainer, Tbody, Td, Th, Tr, } from "@chakra-ui/react";
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+// import { useMemo } from "react";
 
 
 export default function Dashboard() {
+//  const data=useMemo();
+  const columns=[
+    {
+      header:"ID",
+      accessorKey:"id",
+    },
+    {
+      header:"Title",
+      accessorKey:"title",
+    },
+    {
+      header:"Price",
+      accessorKey:"price",
+    },
+
+  ];
+  // const table = useReactTable({
+  //   columns,
+  //   data,
+   
+  //   getCoreRowModel: getCoreRowModel(),
+  // });
   return (
     <div>
       <Box display="flex" flexDirection="column" pt="10px" h="50vh" justifyContent="center" bg="lightgray">
@@ -13,6 +37,37 @@ export default function Dashboard() {
        <Box bg='teal' height='80px'>Total Products</Box>
        <Box bg='teal' height='80px'>Total Reviews</Box>
        </SimpleGrid>
+       <TableContainer>
+        <Table colorScheme="teal">
+          <TableCaption>Product Added Sucessfully</TableCaption>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <Th key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </Th>
+              ))}
+            </Tr>
+          ))}
+
+          <Tbody>
+            {table.getRowModel().rows.map((row) => (
+              <Tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <Td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </Td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+
+       
       </Box>
     </div>
   )
